6264b457bec02fb92a3bafe9f6bb08516ee24e24
Carmen Grantham <carmen@carmengrantham.com>
2015-02-12 10:51:47 +1030

    Fixed up some encoded characters
    A copy and paste that caused some characters to be encoded when they
    didn't need to be.

--- src/main/java/projecteuler/problems/numbers/LargestProductInSeries.java
+++ src/main/java/projecteuler/problems/numbers/LargestProductInSeries.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Problem: The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+ * Problem: The four adjacent digits in the 1000-digit number that have the greatest product are 9 x 9 x 8 x 9 = 5832.
  *      73167176531330624919225119674426574742355349194934
  *      96983520312774506326239578318016984801869478851843
  *      85861560789112949495459501737958331952853208805511
